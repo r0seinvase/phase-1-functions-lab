fb359d75a83c091461dc0883dc6b4291679bc36a
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-24 09:57:49 -0600

    commit tree 020487abe7db4750b064cc6db138a3a007626ad5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    const headquarters = 42
+    const distance = Math.abs(pickup - headquarters);
+    return distance;
+}
+
+function distanceFromHqInFeet(pickup){
+    distanceFromHqInBlocks(pickup);
+    const blocks = distanceFromHqInBlocks(pickup) * 264;
+    return blocks;
+}
+function distanceTravelledInFeet(start,destination) {
+    const travel = (destination - start) * 264;
+    return Math.abs(travel);
+}
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination)
+    if (feet <= 400){
+        return 0;
+    } else if (feet > 400  && feet <=2000) {
+        return (feet - 400) * .02;
+    } else if (feet > 2500) {
+        return "cannot travel that far";
+    } else if (feet >2000) {
+        return 25;
+    }
+}
